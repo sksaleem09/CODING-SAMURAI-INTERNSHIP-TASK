b086ee2053151fb0a125b2f1cfd406dceb81cd11
Sk Saleem <sksaleem221004@gmail.com>
2026-01-17 22:16:24 +0530

    Implement basic calculator functionality in Java

---
+++ Task-1/Simple-Calculator/Calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char operator;
+        double num1, num2, result;
+
+        System.out.print("Enter first number: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = sc.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Cannot divide by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
